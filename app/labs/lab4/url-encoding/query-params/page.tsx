6d747958df0170261eb5a4558a7007c73031f5b7
Nicholas Nguyen <255169637+nicholasn9876@users.noreply.github.com>
2026-03-15 23:51:28 -0400

    fix query-params

--- app/labs/lab4/url-encoding/query-params/page.tsx
+++ app/labs/lab4/url-encoding/query-params/page.tsx
@@ -1,13 +1,11 @@
 "use client";
 
-import { Suspense } from "react";
-import { useSearchParams } from "next/navigation";
 
+import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 
 export default function QueryCalculator() {
-<Suspense>
  const searchParams = useSearchParams();
-</Suspense>
 
 
  const aRaw = searchParams.get("a") || "0";
@@ -20,20 +18,23 @@ export default function QueryCalculator() {
 
 
  return (
-   <div style={{ padding: 40 }}>
-     <h1>Calculator – Query Parameters</h1>
+  <Suspense>
+    <div style={{ padding: 40 }}>
+      <h1>Calculator – Query Parameters</h1>
 
 
-     Raw query values (already decoded by Next.js):
-     <p>
-       a = <code>{aRaw}</code>
-     </p>
-     <p>
-       b = <code>{bRaw}</code>
-     </p>
+      Raw query values (already decoded by Next.js):
+      <p>
+        a = <code>{aRaw}</code>
+      </p>
+      <p>
+        b = <code>{bRaw}</code>
+      </p>
 
 
-     <h2 style={{ color: "green" }}>Sum = {sum}</h2>
-   </div>
+      <h2 style={{ color: "green" }}>Sum = {sum}</h2>
+    </div>
+   </Suspense>
  );
+ 
 }
